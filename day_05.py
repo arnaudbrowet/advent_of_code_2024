82693872d5e418feeb1de81ecae18415f97e35b5
Arnaud Browet <arnaudbrowet@Arnauds-MacBook-Pro.local>
2024-12-13 10:56:30 +0100

    day 05 - part 2

--- day_05.py
+++ day_05.py
@@ -2,9 +2,14 @@ from utils import read_input
 from pathlib import Path
 import math
 
-data = read_input(Path('./data/day_05/input.txt'))
+testing = False
+current_file = "day_05"
+data_path = Path(f'./data/{current_file}/input.txt')
 
-data = '''
+if not testing:
+    data = read_input(data_path)
+else:
+    data = '''
 47|53
 97|13
 97|61
@@ -68,3 +73,32 @@ mid_page = [page[math.floor(len(page)/2)]
 
 solution = sum(mid_page)
 print(f'Part 1 - solution: {solution}')
+
+# Part 2
+invalid_pages = [p for p in pages if not is_valid_page(p)]
+
+
+def sort_page(page, seq):
+    if len(page) == 0:
+        return seq
+
+    current_page = page[0]
+    follow = page_next.get(current_page, [])
+
+    for i, p in enumerate(seq):
+        # break
+        if p in follow:
+            return sort_page(page[1:], seq[0:i]+[current_page]+seq[i:])
+    return sort_page(page[1:], seq+[current_page])
+
+
+sorted_pages = [sort_page(page[1:], [page[0]]) for page in invalid_pages]
+mid_sorted_pages = [page[math.floor(len(page)/2)]
+                    for page in sorted_pages]
+solution = sum(mid_sorted_pages)
+print(f'Part 2 - solution: {solution}')
+
+
+# page = invalid_pages[0]
+# seq=[page[0]]
+# page=page[1:]
